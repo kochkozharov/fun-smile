a8d6be4464fb63e7a9065178af0b0567f6e0c860
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2024-05-09 05:16:52 +0300

    add files

---
+++ Main.fs
@@ -0,0 +1,19 @@
+open Interpreter
+open Parser
+open System.IO
+
+let readTextFromFile (filePath: string) =
+    try
+        File.ReadAllText(filePath)
+    with
+    | :? System.IO.FileNotFoundException ->
+        failwithf "File not found: %s" filePath
+    | ex ->
+        failwithf "Error reading file: %s" (ex.ToString())
+
+[<EntryPoint>]
+let main argv =
+    let filePath = "test.smile"
+    let fileContent = readTextFromFile filePath
+    printExpr (exec (getResult fprogram fileContent ))
+    0 
